1faf8b88886a0ed6f65a729faa0a9bf7eea5dd5a
mgravemeyer <m.gravemeyer@icloud.com>
2020-12-18 08:51:05 +0100

    Initial Commit

---
+++ TCP-Socket/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TCP-Socket
+//
+//  Created by Maximilian Gravemeyer on 18.12.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCP-Socket/TCP_SocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCP_SocketApp.swift
+//  TCP-Socket
+//
+//  Created by Maximilian Gravemeyer on 18.12.20.
+//
+
+import SwiftUI
+
+@main
+struct TCP_SocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
